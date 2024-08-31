7a0b8a57030354c6c02da69483b3d2b96320d9f6
Prasad Patharvat <106762130+prasadp1307@users.noreply.github.com>
2024-08-31 16:52:50 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,48 +1,70 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv').config();
 const path = require('path');
-const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
 
-// Routes Require
+const app = express();
+
+// Importing DB
 const sequelize = require('./database/db');
-const userRoutes = require('./routes/userRoutes');
-
-
-// DataBase Tables
 const User = require('./models/User');
-
-const app = express()
-app.use(express());
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json({ extended: true }));
+const Recipe = require('./models/recipes');
+const Follow = require('./models/follow');
+const Save = require('./models/save');
 
 // Routes
-app.use('/user', userRoutes);
+const accountRoute = require('./routes/accountRoutes');
+const userRoute = require('./routes/userRoutes');
+const uploadRoutes = require('./routes/uploadRoutes'); // Uncomment if needed
+const recipeRoute = require('./routes/recipesRoutes');
+const ratingRoute = require('./routes/ratingRoutes');
+const adminRoute = require('./routes/adminRoutes');
+
+// Middleware
+app.use(cors({
+    origin: '*' // Consider restricting this in production
+}));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Serve static files from the views directory if necessary
 app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+// Serve the index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views', 'index.html'));
-  });
-
-// Error in middleware
-app.use((err, req, res, next) => {
-    console.error('Error:', err);
-    res.status(500).send({ message: 'Internal Server Error' });
-    next();
-  });
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
 
-// Server & Database Start
-sequelize.sync()
-  .then(() => {
-    app.listen(4000, () => {
-      console.log('Server is running on port 4000');
-    });
-  })
-  .catch((err) => {
-    console.error('Error syncing with the database', err);
-  });
+
+// Connect routes
+app.use('/admin', adminRoute);
+app.use('/rate', ratingRoute);
+app.use('/home', recipeRoute);
+app.use('/user', userRoute);
+app.use('/account', accountRoute);
+// app.use('/upload', uploadRoutes); // Uncomment if needed
+
+// DB relations
+User.hasMany(Recipe, { foreignKey: 'userId' });
+Recipe.belongsTo(User, { foreignKey: 'userId' });
+
+User.hasMany(Save, { foreignKey: 'userId' });
+Save.belongsTo(User, { foreignKey: 'userId' });
+
+Recipe.hasMany(Save, { foreignKey: 'recipeId' });
+Save.belongsTo(Recipe, { foreignKey: 'recipeId' });
+
+// Sync database and start server
+sequelize.sync({ force: false, alter: false })
+    .then(() => {
+        app.listen(4000, () => {
+            console.log('Server is running on port 4000');
+        });
+    })
+    .catch(err => console.error('Failed to sync database:', err));
