7281c6ec70b2db2d65f3eea19765e06b9828b912
Prasad Patharvat <106762130+prasadp1307@users.noreply.github.com>
2024-08-31 16:56:20 +0530

    Updated Final Complete

---
+++ views/recipeDetails.js
@@ -0,0 +1,170 @@
+const recipeDetailContainer = document.getElementById('recipeDetailContainer');
+const urlParams = new URLSearchParams(window.location.search);
+const recipeId = urlParams.get('id');
+const token = localStorage.getItem('token');
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    await getRecipeDetail(recipeId);
+    setupEventListeners();
+});
+
+async function getRecipeDetail(id) {
+    try {
+        const response = await axios.get(`http://localhost:4000/home/recipes/${id}`, {
+            headers: { "Authorization": `Bearer ${token}` }
+        });
+        if (response) {
+            // console.log(response.data.isFollowing);
+            const { recipe, isFollowing, loggedInUserId } = response.data
+            // console.log(`>>>>>><<<<<<<<`,recipe,isFollowing,loggedInUserId)
+            displayRecipeDetail(recipe, isFollowing, loggedInUserId);
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+function displayRecipeDetail(recipe, isFollowing, loggedInUserId) {
+    console.log(recipe);
+     // Convert binary data to base64
+     const base64Image = recipe.imgData ? `data:image/jpeg;base64,${arrayBufferToBase64(recipe.imgData.data)}` : 'default-image.jpg';
+    recipeDetailContainer.innerHTML = `
+        <h2>${recipe.recipesName}</h2>
+        <img src="${base64Image}" alt="${recipe.recipesName}" style="width: 400px; height:auto">
+        <p>Cuisine: ${recipe.cuisine}</p>
+        <p>Type: ${recipe.recipesType}</p>
+        <p>Ingredients:</p>
+        <p>${recipe.ingredients}</p>
+        <p>Method:</p>
+        <p>${recipe.method}</p>
+        <p>Cooking Time: ${recipe.cookingTime} minutes</p>
+        <p>Uploaded by: ${recipe.User.name}</p>
+        <p>Average Rating: <span id="averageRating">Loading...</span></p>
+        <div id="ratingSection" style="display: ${recipe.UserId === loggedInUserId ? 'none' : 'block'};">
+            <label for="rating">Rate this recipe:</label>
+            <select id="rating" name="rating">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+            <button id="submitRating">Submit Rating</button>
+        </div>
+        <button id="followButton-${recipe.UserId}" style="display:none;">Follow</button>
+        <button id="unfollowButton-${recipe.UserId}" style="display:none;">Unfollow</button>
+    `;
+
+    const followButton = document.querySelector(`#followButton-${recipe.UserId}`);
+    const unfollowButton = document.querySelector(`#unfollowButton-${recipe.UserId}`);
+
+    if (recipe.UserId != loggedInUserId) {
+        if (isFollowing) {
+            unfollowButton.style.display = 'block';
+        } else {
+            followButton.style.display = 'block';
+        }
+    }
+
+    function arrayBufferToBase64(buffer) {
+        let binary = '';
+        const bytes = new Uint8Array(buffer);
+        const len = bytes.byteLength;
+        for (let i = 0; i < len; i++) {
+            binary += String.fromCharCode(bytes[i]);
+        }
+        return window.btoa(binary);
+    }
+
+
+
+    getRecipeRating(recipe.id);
+
+    document.getElementById('submitRating').addEventListener('click', () => {
+        const rating = document.getElementById('rating').value;
+        rateRecipe(recipe.id, loggedInUserId, rating);
+    });
+
+    followButton.addEventListener('click', () => followUser(recipe.UserId));
+    unfollowButton.addEventListener('click', () => unfollowUser(recipe.UserId));
+}
+
+
+async function getRecipeRating(recipeId) {
+    try {
+        const response = await axios.get(`http://localhost:4000/rate/rating/${recipeId}`, {
+            headers: { "Authorization": `Bearer ${token}` }
+        });
+        console.log(response.data.averageRating);
+        document.getElementById('averageRating').innerText = response.data.averageRating.toFixed(1);
+    } catch (err) {
+        console.log('Error fetching rating:', err);
+    }
+}
+
+async function rateRecipe(recipeId, userId, rating) {
+    try {
+        await axios.post('http://localhost:4000/rate/rate', { userId, recipeId, rating }, {
+            headers: { "Authorization": `Bearer ${token}` }
+        });
+        alert('Rating submitted successfully');
+        getRecipeRating(recipeId);
+    } catch (err) {
+        console.error('Error submitting rating:', err);
+    }
+}
+
+
+async function followUser(userId) {
+    try {
+        console.log(userId);
+        const response = await axios.post(`http://localhost:4000/home/follow/${userId}`, {}, {
+            headers: { "Authorization": `Bearer ${token}` }
+        });
+        console.log(response.data.message);
+
+        document.querySelector(`#followButton-${userId}`).style.display = 'none';
+        document.querySelector(`#unfollowButton-${userId}`).style.display = 'block';
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function unfollowUser(userId) {
+    try {
+        const response = await axios.post(`http://localhost:4000/home/unfollow/${userId}`, {}, {
+            headers: { "Authorization": `Bearer ${token}` }
+        });
+        console.log(response.data.message);
+
+        document.querySelector(`#followButton-${userId}`).style.display = 'block';
+        document.querySelector(`#unfollowButton-${userId}`).style.display = 'none';
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+function setupEventListeners() {
+    document.getElementById('saveToFavorites').addEventListener('click', showCollectionOptions);
+    document.getElementById('confirmSave').addEventListener('click', saveToCollection);
+}
+
+function showCollectionOptions() {
+    document.getElementById('collectionSelect').style.display = 'block';
+    document.getElementById('confirmSave').style.display = 'block';
+}
+
+async function saveToCollection() {
+    const selectedCollection = document.getElementById('collectionSelect').value;
+    try {
+        await axios.post(`http://localhost:4000/user/save`, { recipeId, collection: selectedCollection }, {
+            headers: { "Authorization": `Bearer ${token}` }
+        });
+        alert(`Recipe saved to ${selectedCollection}`);
+        document.getElementById('collectionSelect').style.display = 'none';
+        document.getElementById('confirmSave').style.display = 'none';
+    } catch (err) {
+        console.error('Error saving to collection:', err);
+    }
+}
---
+++ views/user.js
@@ -0,0 +1,87 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    const token = localStorage.getItem('token');
+
+    try {
+        const userResponse = await axios.get('http://localhost:4000/user/profile', {
+            headers: { "Authorization": `Bearer ${token}` }
+        });
+
+        const { user, recipes } = userResponse.data;
+        document.getElementById('username').innerText = user.name;
+        document.getElementById('emailId').innerText = user.email;
+
+        const recipesContainer = document.getElementById('recipesContainer');
+
+        recipes.forEach(recipe => {
+            const base64Image = recipe.imgData ? `data:image/jpeg;base64,${arrayBufferToBase64(recipe.imgData.data)}` : 'default-image.jpg';
+            const recipeCard = document.createElement('div');
+            recipeCard.className = 'recipe-card';
+            recipeCard.innerHTML = `
+             <img src="${base64Image}" alt="${recipe.recipesName}" style="width: 200px; height:auto">
+                <h3>${recipe.recipesName}</h3>
+                <div class="buttons">
+                    <button onclick="editRecipe(${recipe.id})">Edit</button>
+                    <button class="delete-button" onclick="deleteRecipe(${recipe.id})">Delete</button>
+                </div>
+            `;
+            recipesContainer.appendChild(recipeCard);
+        });
+    } catch (err) {
+        console.error('Error fetching profile data:', err);
+    }
+});
+
+
+function arrayBufferToBase64(buffer) {
+    let binary = '';
+    const bytes = new Uint8Array(buffer);
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+}
+
+async function editRecipe(recipeId) {
+    localStorage.setItem('recipeIdToEdit', recipeId);
+    window.location.href = './recipesForm.html';
+
+}
+
+async function deleteRecipe(recipeId) {
+    const token = localStorage.getItem('token');
+    try {
+        await axios.delete(`http://localhost:4000/home/${recipeId}`, {
+            headers: { "Authorization": `Bearer ${token}` }
+        });
+        document.location.reload();
+    } catch (err) {
+        console.error('Error deleting recipe:', err);
+    }
+}
+async function loadFavorites(collectionName) {
+    const token = localStorage.getItem('token');
+    try {
+        const favoritesResponse = await axios.get(`http://localhost:4000/user/${collectionName}`, {
+            headers: { "Authorization": `Bearer ${token}` }
+        });
+
+        const favorites = favoritesResponse.data.favorites;
+        const favoritesContainer = document.getElementById('favoritesContainer');
+        favoritesContainer.innerHTML = '';
+
+        favorites.forEach(favorite => {
+            // Correct the use of the 'favorite' object
+            const base64Image = favorite.Recipe.imgData ? `data:image/jpeg;base64,${arrayBufferToBase64(favorite.Recipe.imgData.data)}` : 'default-image.jpg';
+            const favoriteCard = document.createElement('div');
+            favoriteCard.className = 'favorite-card';
+            favoriteCard.innerHTML = `
+                <img src="${base64Image}" alt="${favorite.Recipe.recipesName}" style="width: 200px; height:auto">
+                <h3>${favorite.Recipe.recipesName}</h3>
+            `;
+            favoritesContainer.appendChild(favoriteCard);
+        });
+    } catch (err) {
+        console.error('Error fetching favorites:', err);
+    }
+}
